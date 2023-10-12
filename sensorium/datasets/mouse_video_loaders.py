9847a4f3e1252bc09e434dde788ae2a2cdeaec30
yongrong.qiu <yongrong.qiu@uni-goettingen.de>
2023-10-12 21:55:19 +0200

    Not use random sampling for training data

--- sensorium/datasets/mouse_video_loaders.py
+++ sensorium/datasets/mouse_video_loaders.py
@@ -1,19 +1,12 @@
 import numpy as np
 from neuralpredictors.data.datasets import MovieFileTreeDataset
 from neuralpredictors.data.samplers import SubsetSequentialSampler
-from neuralpredictors.data.transforms import (
-    AddBehaviorAsChannels,
-    AddPupilCenterAsChannels,
-    ChangeChannelsOrder,
-    CutVideos,
-    ExpandChannels,
-    NeuroNormalizer,
-    ScaleInputs,
-    SelectInputChannel,
-    Subsample,
-    Subsequence,
-    ToTensor,
-)
+from neuralpredictors.data.transforms import (AddBehaviorAsChannels,
+                                              AddPupilCenterAsChannels,
+                                              ChangeChannelsOrder, CutVideos,
+                                              ExpandChannels, NeuroNormalizer,
+                                              ScaleInputs, SelectInputChannel,
+                                              Subsample, Subsequence, ToTensor)
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 
@@ -34,7 +27,6 @@ def mouse_video_loader(
     include_pupil_centers_as_channels=False,
     scale=1,
     to_cut=True,
-    random_sample_within_snippet_flag=False,
 ):
     """
     Symplified version of the sensorium mouse_loaders.py
@@ -88,10 +80,10 @@ def mouse_video_loader(
             ChangeChannelsOrder((1, 0), in_name="responses"),
         ]
         if include_behavior:
-            more_transforms.append(ChangeChannelsOrder((1, 0), in_name="behavior"))
+            more_transforms.append( ChangeChannelsOrder((1, 0), in_name="behavior") )
         if include_pupil_centers:
-            more_transforms.append(ChangeChannelsOrder((1, 0), in_name="pupil_center"))
-
+            more_transforms.append( ChangeChannelsOrder((1, 0), in_name="pupil_center") )
+        
         if to_cut:
             more_transforms.append(
                 Subsequence(frames=frames, channel_first=(), offset=offset)
@@ -101,12 +93,16 @@ def mouse_video_loader(
             ExpandChannels("videos"),
         ]
         if include_behavior:
-            more_transforms.append(ChangeChannelsOrder((1, 0), in_name="behavior"))
+            more_transforms.append( ChangeChannelsOrder((1, 0), in_name="behavior") )
         if include_pupil_centers:
-            more_transforms.append(ChangeChannelsOrder((1, 0), in_name="pupil_center"))
+            more_transforms.append( ChangeChannelsOrder((1, 0), in_name="pupil_center") )
 
         if include_behavior:
-            more_transforms.append(AddBehaviorAsChannels("videos"))
+            more_transforms.append(
+                AddBehaviorAsChannels(
+                    "videos"
+                )
+            )
         if include_pupil_centers and include_pupil_centers_as_channels:
             more_transforms.append(AddPupilCenterAsChannels("videos"))
 
@@ -133,72 +129,26 @@ def mouse_video_loader(
 
         dat2.transforms.extend(more_transforms)
 
-        if random_sample_within_snippet_flag == False:
-            # subsample images
-            tier = None
-            dataloaders = {}
-            keys = [tier] if tier else list(set(list(dat2.trial_info.tiers)))
-            tier_array = dat2.trial_info.tiers
+        # subsample images
+        tier = None
+        dataloaders = {}
+        keys = [tier] if tier else list(set(list(dat2.trial_info.tiers)))
+        tier_array = dat2.trial_info.tiers
 
-            for tier in keys:
-                if tier != "none":
-                    subset_idx = np.where(tier_array == tier)[0]
+        for tier in keys:
+            if tier != 'none':
+                subset_idx = np.where(tier_array == tier)[0]
 
-                    sampler = (
-                        SubsetRandomSampler(subset_idx)
-                        if tier == "train"
-                        else SubsetSequentialSampler(subset_idx)
-                    )
-                    dataloaders[tier] = DataLoader(
-                        dat2,
-                        sampler=sampler,
-                        batch_size=batch_size,
-                    )
-
-        else:  # perform the random sampling within each snippet
-            newtiers = []  # tiers for dat3
-            newinds = []  # index of dat2 for dat3
-            NumRandomSubSequence = 5  # 40
-            SubSequenceLength = 100
-            for ii, tier in enumerate(dat2.trial_info.tiers):
-                if tier != "none":  # if tier!='none' and ii<15:
-                    # print (ii, dat2[ii]._fields, tier)
-                    if tier == "train":
-                        newtiers.extend(["train"] * NumRandomSubSequence)
-                        newinds.extend([ii] * NumRandomSubSequence)
-                    else:
-                        newtiers.append(tier)
-                        newinds.append(ii)
-            # print (f'len(newtiers): {len(newtiers)}, newtiers[:50]: {newtiers[:50]}')
-            # print (f'len(newinds): {len(newinds)}, newinds[:50]: {newinds[:50]}')
-
-            np.random.seed(10)
-            RandomSart = np.random.randint(
-                low=0, high=300 - SubSequenceLength, size=NumRandomSubSequence
-            )
-            # print (f'RandomSart: {RandomSart}')
-
-            dat3 = NRandomSubSequence_dataset(
-                dat2, newtiers, newinds, RandomSart, SubSequenceLength
-            )
-
-            tier = None
-            dataloaders = {}
-            keys = [tier] if tier else list(set(list(dat2.trial_info.tiers)))
-            for tier in keys:
-                if tier != "none":
-                    subset_idx = np.where(np.array(newtiers) == tier)[0]
-                    sampler = (
-                        SubsetRandomSampler(subset_idx)
-                        if tier == "train"
-                        else SubsetSequentialSampler(subset_idx)
-                    )
-                    batch_size = 8 if tier == "train" else 3
-                    dataloaders[tier] = DataLoader(
-                        dat3,
-                        sampler=sampler,
-                        batch_size=batch_size,
-                    )
+                sampler = (
+                    SubsetRandomSampler(subset_idx)
+                    if tier == "train"
+                    else SubsetSequentialSampler(subset_idx)
+                )
+                dataloaders[tier] = DataLoader(
+                    dat2,
+                    sampler=sampler,
+                    batch_size=batch_size,
+                )
 
         dataset_name = path.split("/")[-2]
         for k, v in dataloaders.items():
@@ -207,43 +157,3 @@ def mouse_video_loader(
             dataloaders_combined[k][dataset_name] = v
 
     return dataloaders_combined
-
-
-from torch.utils.data import Dataset
-from collections import namedtuple
-
-
-class NRandomSubSequence_dataset(Dataset):
-    def __init__(self, dat2, newtiers, newinds, RandomSart, SubSequenceLength):
-        self.dat2 = dat2
-        self.newtiers = newtiers
-        self.newinds = newinds
-        self.RandomStart = RandomSart
-        self.num4rand = len(self.RandomStart)
-        self.RandomEnd = self.RandomStart + SubSequenceLength
-
-    def __getitem__(self, index):
-        # datapoint = namedtuple("DataPoint", self.dat2.data_keys)
-        if self.newtiers[index] == "train":
-            return self.dat2[self.newinds[index]].__class__(
-                **{
-                    k: getattr(self.dat2[self.newinds[index]], k)[
-                        :,
-                        self.RandomStart[index % self.num4rand] : self.RandomEnd[
-                            index % self.num4rand
-                        ],
-                    ]
-                    for k in self.dat2[self.newinds[index]]._fields
-                }
-            )
-
-        else:
-            return self.dat2[self.newinds[index]]
-        # {k: getattr(self.dat2[ self.newinds[index] ],k) for k in self.dat2[ self.newinds[index] ]._fields}
-
-    def __len__(self):
-        return len(self.newtiers)
-
-    @property
-    def neurons(self):
-        return self.dat2.neurons
