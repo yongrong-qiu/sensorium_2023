0391fe6ac4c13d5b3f13bf5114491140023e7069
yongrong.qiu <qyrixpress@gmail.com>
2025-09-04 01:08:36 +0000

    Minor update ReduceLROnPlateau() for newer Pytorch

--- sensorium/training/video_training_loop.py
+++ sensorium/training/video_training_loop.py
@@ -186,7 +186,7 @@ def standard_trainer(
         patience=patience,
         threshold=tolerance,
         min_lr=min_lr,
-        verbose=verbose,
+        # verbose=verbose,
         threshold_mode="abs",
     )
 
